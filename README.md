38b1394fa9643f457049b05a5d2eb14526dce77b
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 <vins25165@gmail.com>
2021-10-11 21:47:17 +0800

    fix: menganti kartu tips dan menghapus tulisan pendukung

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Infocovidid.github.io
 Website ini memberikan informasi tentang Covid 19 yang ada di indonesia secara terupdate
 
-# thanks to
+# makasi untuk...
 [covid api](https://apicovid19indonesia-v2.vercel.app/api)
+[vaksinasi api](https://vaksincovid19-api.vercel.app/api/vaksin)
--- js/script.js
+++ js/script.js
@@ -3,8 +3,7 @@ window.onload = () => {
 		method: "GET"
 	}).then(res => res.json()).then(res => {
 		let dataTU = getDocById("terakhir-update");
-		let getDataTU = new Date(res.lastUpdate);
-		dataTU.innerHTML = `${getDataTU.getUTCDay()} / ${getDataTU.getUTCMonth()} / ${getDataTU.getUTCFullYear()}`
+		dataTU.innerHTML = res.lastUpdate;
 
 		let sembuh = res.sembuh;
 		let meninggal = res.meninggal;
@@ -20,5 +19,15 @@ window.onload = () => {
 		dataP.innerHTML = positif.toLocaleString();
 		dataM.innerHTML = meninggal.toLocaleString();
 		dataD.innerHTML = dirawat.toLocaleString();
+	})
+
+	fetch("https://vaksincovid19-api.vercel.app/api/vaksin", {
+		method: "GET"
+	}).then(res => res.json()).then(res => {
+		let v1 = getDocById("data-v1");
+		let v2 = getDocById("data-v2");
+
+		v1.innerHTML = res.vaksinasi1.toLocaleString();
+		v2.innerHTML = res.vaksinasi2.toLocaleString();
 	})
 }
